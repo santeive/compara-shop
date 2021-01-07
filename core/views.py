6cd3b7f2fc6c781ae23757845bb9567602031ac9
santeive <packo.ive@gmail.com>
2021-01-06 19:13:11 -0600

    Add Linio to COmpara shop

--- core/views.py
+++ core/views.py
@@ -1,10 +1,36 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
+import sqlite3
 
 class HomePageView(TemplateView):
     
     def get(self, request, *args, **kwargs):
-        return render(request, "core/home.html", {'title':"Larva Platform"})
+        with sqlite3.connect("stores.sqlite3") as conn:
+            cursorObj = conn.cursor()
+            
+            #Products
+            cursorObj.execute('SELECT count(id) FROM linio')
+            products = cursorObj.fetchone()
+
+            #Brands
+            cursorObj.execute('SELECT count(DISTINCT brand) FROM linio')
+            brands = cursorObj.fetchone()
+
+            #Categories
+            cursorObj.execute('SELECT COUNT(DISTINCT category) FROM linio')
+            rows = cursorObj.fetchone()
+
+            #Sellers
+            cursorObj.execute('SELECT COUNT(DISTINCT seller) FROM linio')
+            seller = cursorObj.fetchone()
+
+            context = {
+                'categories': rows[0],
+                'products': products[0],
+                'brands': brands[0],
+                'seller': seller[0]
+            }
+            return render(request, "core/home.html", context)
 
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
--- stores/views.py
+++ stores/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import sqlite3
+
 
 # Create your views here.
 def claroshop(request):
-    records = 24334
-    context = {
-        'records': records
-    }
-    return render(request, 'stores/claroshop.html', context)
+    with sqlite3.connect("stores.sqlite3") as conn:
+        cursorObj = conn.cursor()
+        
+        #Products
+        cursorObj.execute('SELECT count(id) FROM linio')
+        products = cursorObj.fetchone()
+
+        #Brands
+        cursorObj.execute('SELECT count(DISTINCT brand) FROM linio')
+        brands = cursorObj.fetchone()
+
+        #Categories
+        cursorObj.execute('SELECT COUNT(DISTINCT category) FROM linio')
+        rows = cursorObj.fetchone()
+
+        context = {
+            'categories': rows[0],
+            'products': products[0],
+            'brands': brands[0]
+        }
+        return render(request, 'stores/claroshop.html', context)
 
 def linio(request):
     titulo = 'Pagina base'
